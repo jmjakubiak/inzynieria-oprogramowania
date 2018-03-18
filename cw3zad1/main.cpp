fe444c6e4bb06cf947fb3390e737c926d079ea56
Jan <jan.m.jakubiak@gmail.com>
2018-03-18 22:33:00 +0100

    cwiczenia 3 zadanie 1
    cw3 zad1

---
+++ cw3zad1/main.cpp
@@ -0,0 +1,16 @@
+// cwiczenia 3 zadanie 1
+
+#include <iostream>
+#include "TStudent.h"
+#include "TUniversity.h"
+#include "TStudent.cpp"
+#include "TUniversity.cpp"
+
+using namespace std;
+
+int main()
+{
+    TStudent* a = new TStudent();
+    TUniversity* b = new TUniversity();
+    return 0;
+}
